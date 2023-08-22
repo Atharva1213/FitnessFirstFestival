d01f146504378e4f99564dc73b8709e717c5ee8e
AryaK-31 <aryakaslikar31@gmail.com>
2023-08-22 13:15:36 +0530

    Website restricted to only portrait mode on every render

--- src/components/LandscapeModeOverlay.js
+++ src/components/LandscapeModeOverlay.js
@@ -2,15 +2,18 @@ import React from 'react';
 import '../LandscapeModeOverlay.css'; // Import the CSS for styling
 
 const LandscapeModeOverlay = ({ show }) => {
-  if (!show) {
-    return null;
-  }
+    if (!show) {
+        return null;
+    }
+        return (
+            <div className="overlay p-4">
 
-  return (
-    <div className="overlay">
-      Please use portrait mode to view this site.
-    </div>
-  );
+                The website is available on Moblie screens!
+                <br />
+                The Site is still underdevelopment for desktop and laptop screens.
+                Thank You For your Patience
+            </div>
+        );
 };
 
 export default LandscapeModeOverlay;

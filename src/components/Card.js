f8f70befaf819c8ba5e7212c66d0edc355a542f1
Atharva Dilip Lende <127081975+Atharva1213@users.noreply.github.com>
2023-08-25 20:03:03 +0530

    Update Card.js
    Change the Src address giving type

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card(props) {
     return (
     <div className="max-w-sm bg-white shadow flex flex-col">
-    <div className='h-[55%] w-[100%] bg-black bg-cover bg-center' style={{ backgroundImage: `url(${props.cardImg})` }}>
+    <div className='h-[55%] w-[100%] bg-black bg-cover bg-center' style={{ backgroundImage: `url(data:image/jpeg;base64,${props.cardImg})` }}>
 
     </div>
     <div className="pl-5 pr-1 h-[45%] w-full flex flex-col justify-center items-start bg-slate-900">

92a8a31b5b859e09d169966365136e5ee6700ff8
Atharva Dilip Lende <127081975+Atharva1213@users.noreply.github.com>
2023-08-25 20:04:33 +0530

    Update Gallery.js
    Gallery dynamic nature

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,82 +1,27 @@
-import React from "react";
-import gallery1 from "../Images/gallery1.jpg";
-import gallery2 from "../Images/gallery2.jpg";
-import gallery3 from "../Images/gallery3.jpg";
-import gallery4 from "../Images/gallery4.jpg";
-import gallery5 from "../Images/gallery5.jpg";
-import gallery6 from "../Images/gallery6.jpg";
-import gallery7 from "../Images/gallery7.jpg";
-import gallery8 from "../Images/gallery8.jpg";
-// ... import other images
-
-const galleryData = [
-  {
-    backgroundImage: gallery1,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-    unHide: true
-  },
-  {
-    backgroundImage: gallery2,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery3,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery4,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery5,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery6,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery7,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery8,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery1,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery2,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery1,
-    title: "Marathon - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-  {
-    backgroundImage: gallery2,
-    title: "CrossFit - 27/08/2023 - 14:00 PM IST",
-    venue: "Venue: Location",
-  },
-];
+import React,{useEffect,useState} from "react";
+import axios  from "axios";
 
 function Gallery() {
+  
+const  [gallery_data,setgallery]=useState([]);
+
+//Function of api called ;
+const Get_API_Called=()=>{ 
+ axios.get("http://localhost:80/first/Gallery.php")
+ .then((res)=>
+  {  
+     setgallery(res.data);
+  })}
+
+//when page loaded then function get excute 
+useEffect(() => {
+ // Fetch data from the API when the page loads
+ Get_API_Called();
+}, []);
+
   return (
     <div id="gallery" className="overflow-hidden h-auto w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 grid-rows-3">
-      {galleryData.map((item, index) => (
+      {gallery_data.map((item, index) => (
         item.unHide ? <div key={index} className="flex justify-center items-center h-[25vh] md:h-[25vh] lg:h-[30vh] xl:h-[40vh] w-full bg-pink-900">
           <div data-aos="fade-right" data-aos-offset="200" data-aos-duration="1300" className='flex flex-col items-center gap-4'>
             <h1  className='text-3xl lg:text-5xl font-semibold text-center mx-auto text-white font-bannerFont2'>
@@ -88,11 +33,11 @@ function Gallery() {
           <div
             key={index} data-aos="fade-up" data-aos-offset="200" data-aos-duration="1300"
             className="relative bg-cover bg-center h-[25vh] md:h-[25vh] lg:h-[30vh] xl:h-[40vh] w-full"
-            style={{ backgroundImage: `url(${item.backgroundImage})` }}
+            style={{ backgroundImage: `url(data:image/jpeg;base64,${item.Image}` }}
           >
             <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[#110e0e9d] opacity-0 transition duration-300 ease-in-out hover:opacity-100 text-center ">
-              <p className="text-white text-center mx-auto text-xl mt-[25%]">{item.title}</p>
-              <p className="text-white text-center mx-auto text-xl mt-4">{item.venue}</p>
+              <p className="text-white text-center mx-auto text-xl mt-[25%]">{item.Title}</p>
+              <p className="text-white text-center mx-auto text-xl mt-4">{item.Venue}</p>
             </div>
           </div>
       ))}
